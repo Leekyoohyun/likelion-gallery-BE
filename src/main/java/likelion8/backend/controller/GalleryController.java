6be117732a9181ff64f2415b1c93fc5296d587a6
kyoohyunlee <khyhlee@naver.com>
2025-05-20 22:01:01 +0900

    Gallery 컨트롤러 생성

---
+++ src/main/java/likelion8/backend/controller/GalleryController.java
@@ -0,0 +1,11 @@
+package likelion8.backend.controller;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GalleryController {
+
+}
